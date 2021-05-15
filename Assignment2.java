177a98cd1d5fdb4c5bd52f4bf8f5383234ee5016
neil245 <indranilghoshal5@gmail.com>
2021-05-15 17:22:48 +0530

    New files added

--- Assignment2.java
+++ Assignment2.java
@@ -1,9 +1,12 @@
 package com.Assignment2.java;
 import java.util.Scanner;
+import java.util.Random;
 public class Assignment2 {
 	public static void main(String[] args) {
 		
-		int length;
+		int length1;
+		int length2;
+		System.out.println("Coordinates of the first line");
 		System.out.println("Enter x1");
 		Scanner sc = new Scanner(System.in);
 		int x1 = sc.nextInt();
@@ -17,9 +20,29 @@ public class Assignment2 {
 		System.out.println("x2 : " + x2 );
 		System.out.println("y1 : " + y1 );
 		System.out.println("y2 : " + y2 );
-		length = (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1);
-		System.out.println(length);
-		
+		length1 = (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1);
+		System.out.println(length1);
+		System.out.println("Coordinates of the second line");
+		System.out.println("Enter x11");
+		int x11 = sc.nextInt();
+		System.out.println("Enter x22");
+		int x22 = sc.nextInt();
+		System.out.println("Enter y11");
+		int y11 = sc.nextInt();
+		System.out.println("Enter y22");
+		int y22 = sc.nextInt();
+		System.out.println("x11 : " + x11 );
+		System.out.println("x22 : " + x22 );
+		System.out.println("y11 : " + y11 );
+		System.out.println("y22 : " + y22 );
+		length2 = (x22 - x11)*(x22 - x11) + (y22 - y11)*(y22 - y11);
+		System.out.println(length2);
+		Integer val1 = new Integer(length1);
+	    Integer val2 = new Integer(length2);
+	    System.out.println(val1.equals(val2));
+	    
 	}
 
 }
+
+	
